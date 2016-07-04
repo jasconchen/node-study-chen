d7ce04d3b01d883a556d9d1e1b6367368e237459
jasconchen <jasonchenzehui@gmail.com>
2016-07-04 17:27:03 +0800

    test

--- HelloWord/moduledemo/student.js
+++ HelloWord/moduledemo/student.js
@@ -1,12 +1,3 @@
-/*
- * @Author: USER
- * @Date:   2016-07-03 11:43:46
- * @Last Modified by:   USER
- * @Last Modified time: 2016-07-03 11:45:00
- */
-
-'use strict';
-
 function add(student) {
     console.log('Add student:' + student)
 }
--- HelloWord/moduledemo/teacher.js
+++ HelloWord/moduledemo/teacher.js
@@ -1,12 +1,3 @@
-/*
-* @Author: USER
-* @Date:   2016-07-03 11:45:21
-* @Last Modified by:   USER
-* @Last Modified time: 2016-07-03 11:45:50
-*/
-
-'use strict';
-
 function add(teacher) {
     console.log('Add Teacher:' + teacher)
 }
